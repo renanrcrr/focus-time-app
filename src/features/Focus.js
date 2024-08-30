2bc581f3be52ead48f31a69a9e8c45976df1f66c
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-30 12:32:05 +0930

    Adding text input from react native paper.

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-paper';
 import { colors } from '../utils/colors';
 
 export const Focus = () => (
   <View style={styles.container}>
-    <Text style={styles.text}>Focus Feature</Text>
+    <View style={styles.inputContainer}>
+      <TextInput label="What would you like to focus on?" />
+    </View>
   </View>
 )
 
@@ -12,6 +15,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   }, 
+  inputContainer: {
+    flex: 0.5,
+    padding: 25,
+    justifyContent: 'top'
+  },
   text: {
     color: colors.white
   }
