68331da10299817c830a6aa52c56e151d75ca7e3
renanrcrr <renanrcrr.developer@gmail.com>
2024-08-31 14:51:10 +0930

    Hooking up the countdown.

--- src/features/Timer.js
+++ src/features/Timer.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Countdown } from '../components/Countdown';
+import { RoundedButton } from '../components/RoundedButton';
 
-export const Timer = ({ focusSubject }) => (
-  <View>
-    <Text>
-      Focus feature {focusSubject}
-    </Text>
-  </View> 
-);
+export const Timer = ({ focusSubject }) => {
+  const [isStarted, setIsStarted] = useState(false);
+
+  return (
+    <View>
+      <Text>
+        <View style={styles.container}>
+          <View style={styles.countdown}>
+            <Countdown
+              isPaused={!isStarted}
+              onProgress={() => {}}
+              onEnd={() => {}}
+            />
+          </View>
+          <View style={styles.buttonWrapper}>
+            {!isStarted && (
+              <RoundedButton title="start" onPress={() => setIsStarted(true)} />
+            )}
+            {isStarted && (
+              <RoundedButton title="pause" onPress={() => setIsStarted(false)} />
+            )}
+          </View>
+        </View>
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  countdown: {
+    flex: 0.5,
+    padding: 70,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonWrapper: {
+    flex: 0.3,
+    flexDirection: 'row',
+    padding: 140,
+    alignItems: 'center',
+  },
+});
