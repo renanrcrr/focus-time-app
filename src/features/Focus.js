987058ec74f6c503b27110192dfedef19ad3ebd2
renanrcrr <renanrcrr.developer@gmail.com>
2024-09-07 14:50:00 +0930

    Adding FocusHistory feature.

--- src/features/Focus.js
+++ src/features/Focus.js
@@ -26,7 +26,6 @@ export const Focus = ({ addSubject }) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
   },
   button: {
     justifyContent: 'center',
---
+++ src/features/FocusHistory.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const FocusHistory = () => {
+  return (
+    <View>
+    <Text>Focus history will be build here!</Text>
+    </View>
+  )
+}
